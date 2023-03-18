2c6d40d3931bd59d069373d016da7ae6ab540861
mutai-gilbert <mutaigilbert85@gmail.com>
2023-03-18 18:52:34 +0300

    Fixed the linters

--- src/counter.test.js
+++ src/counter.test.js
@@ -1,10 +1,11 @@
-import addMoviesCounter from './modules/Counter.js'
+import addMoviesCounter from './modules/counter.js';
+
 describe('Items Count', () => {
   const arr = [];
   document.body.innerHTML = `
   <div class="count-movies">
   <span></span>
-  </div>`
+  </div>`;
   test('No items count', () => {
     const tester = addMoviesCounter(arr);
     expect(tester).toBe(0);
