ccba6eea055b883d76bcf92bf41af9a69fbaf83f
Roland-Ntwali <rolandntwali@gmail.com>
2023-03-18 17:20:19 +0200

    Add tests for counter

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,9 +1,8 @@
 const addMoviesCounter = (data) => {
   const countMovies = document.querySelector('.count-movies');
-
   const numberOfMovies = document.createElement('span');
   numberOfMovies.innerHTML = `(${data.length})`;
-
   countMovies.append(numberOfMovies);
+  return data.length;
 };
 export default addMoviesCounter;
